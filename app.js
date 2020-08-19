c3b0dade46bdde70877b6838eba4096487565fa4
Pedro <pedro.bertucci@polygon.pt>
2020-08-19 14:42:17 +0100

    Added database with API RESTFull working

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+    "mongodb+srv://node-shop:" + 
+    process.env.MONGO_ATLAS_PW + 
+    "@node-rest-shop.qtjff.mongodb.net/<dbname>?retryWrites=true&w=majority", 
+    {
+        useNewUrlParser: true,
+        useMongoClient: true,
+        useUnifiedTopology: true
+    }
+);
+
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,7 +34,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         return res.status.json({})
     }
-    
+
     next();
 });
 
