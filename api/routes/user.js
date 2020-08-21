78ff965f474770083f047e7ad85f96c451eef2ed
Pedro <pedro.bertucci@polygon.pt>
2020-08-21 14:24:21 +0100

    Added jwt token on signin user

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
-const { request } = require("express");
 
 router.get("/", (request, response, next) => {
     return response.status(200).json({
@@ -13,7 +13,6 @@ router.get("/", (request, response, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-    console.log(req);
     User.find({email: req.body.email})
     .exec()
     .then(user => {
@@ -56,6 +55,45 @@ router.post("/signup", (req, res, next) => {
     }).catch(err => {
         console.log(err);
         return res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.post("/signin", (req, res, next) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length < 1) {
+            return res.status(404).json({
+                message: "Auth failed"
+            });
+        } else {
+           bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if(err) {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                } else {
+                    if(result) {
+                        const token = jwt.sign({
+                            email: user[0].email,
+                            userId: user[0]._id
+                        }, process.env.JWT_KEY, {
+                            expiresIn: "12h"
+                        });
+
+                        return res.status(200).json({
+                            message: "Auth successful",
+                            token: token
+                        });
+                    }
+                }
+           });  
+        }
+    })
+    .catch(err => {
+        return response.status(500).json({
             error: err
         });
     });
